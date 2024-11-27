ca45caa1d441bd72dc9a574881b8864e01036360
Mitch Nenn <mitch.nenn@earthlink.net>
2024-11-26 22:42:28 -0800

    Rename functions for clarity in DNA base counting
    Renamed the function `countBases` to `countNucleoBases` and adjusted variable names for better readability. This change enhances code comprehensibility by using more descriptive naming conventions.

--- CurrentProblemRunner/Program.fs
+++ CurrentProblemRunner/Program.fs
@@ -10,13 +10,13 @@ let sampleData path = readFileAndStripCR path
                       |> toChars
 
 // Function to count bases in the sample data
-let countBases data baseCh = data
-                             |> Array.filter (fun ch -> ch = baseCh)
-                             |> Array.length
+let countNucleoBases data baseName = data
+                                     |> Array.filter (fun ch -> ch = baseName)
+                                     |> Array.length
 
 // Iterating over base names and using sprintf for each of them
 let baseCounts path = baseNames
-                      |> Array.map (fun baseCh -> sprintf "%d " (countBases (sampleData path) baseCh))
+                      |> Array.map (fun baseName -> sprintf "%d " (countNucleoBases (sampleData path) baseName))
 
 // Printing the results
 let filePath = "input.txt"
