6acb213c79551486f0c57f105852eabac7848f33
Mitch Nenn <mitch.nenn@earthlink.net>
2024-11-26 22:35:32 -0800

    Rework some of the functions.

--- CurrentProblemRunner/Program.fs
+++ CurrentProblemRunner/Program.fs
@@ -1,11 +1,13 @@
 ﻿let baseNames = [|'A'; 'C'; 'G'; 'T' |]
 
-let toChars (aString:string) = aString.ToCharArray()
-let stripCR (aString:string) = aString.Replace("\r\n", "").Trim()
+let toChars (input:string) = input.ToCharArray()
+let stripCR (input:string) = input.Replace("\r\n", "").Trim()
+let readFileAndStripCR (filePath: string) =
+    let fileContent = System.IO.File.ReadAllText(filePath)
+    stripCR fileContent
 
-let sampleData = System.IO.File.ReadAllText("input.txt")
-                 |> stripCR
-                 |> toChars
+let sampleData path = readFileAndStripCR path
+                      |> toChars
 
 // Function to count bases in the sample data
 let countBases data baseCh = data
@@ -13,9 +15,10 @@ let countBases data baseCh = data
                              |> Array.length
 
 // Iterating over base names and using sprintf for each of them
-let baseCounts = baseNames
-                 |> Array.map (fun baseCh -> sprintf "%d " (countBases sampleData baseCh))
+let baseCounts path = baseNames
+                      |> Array.map (fun baseCh -> sprintf "%d " (countBases (sampleData path) baseCh))
 
 // Printing the results
-baseCounts |> Array.iter (printf "%s")
+let filePath = "input.txt"
+baseCounts filePath |> Array.iter (printf "%s")
 printfn ""
