3d3ba47ab442393ca661f2ef0e6f22cad737705d
Mitch Nenn <mitch.nenn@earthlink.net>
2024-11-29 14:33:55 -0800

    Transcribe DNA to RNA success.

--- Rosalind.Functions.Lib/NucleoBase.fs
+++ Rosalind.Functions.Lib/NucleoBase.fs
@@ -10,3 +10,17 @@ module NucleoBase =
                                 |> Array.map (fun baseName -> countCharsByValue (readFromFileAndConvertToChars path) baseName)
                                 |> Array.map string
                                 |> String.concat " "
+
+    let transcribeDnaToRna (inputArray: char array) : char array =
+        let mapping =
+            dict [
+                'A', 'A'
+                'T', 'U'
+                'C', 'C'
+                'G', 'G'
+            ]
+        inputArray |> Array.map (fun c -> mapping.[c])
+        
+    let rec transcribeDnaSeqToRnaSeq(inputString: string) : string =
+        let result = inputString.ToCharArray() |> transcribeDnaToRna
+        System.String(result)
--- TranscribeDNAtoRNG/Program.fs
+++ TranscribeDNAtoRNG/Program.fs
@@ -1,6 +1,14 @@
 ﻿open DefaultNamespace.FileHandling
 open DefaultNamespace.NucleoBase
 
+let sampleInput = readTextFromFile "sample_input.txt"
+printfn "Sample input: %s" sampleInput
+let sampleResult = transcribeDnaSeqToRnaSeq sampleInput
+printfn "Sample result: %s" sampleResult
+let expectedSampleResult = readTextFromFile "sample_output.txt"
+printfn "Expected sample output: %s" expectedSampleResult
 
-
-printfn ""
+let problemInput = readTextFromFile "problem_input.txt"
+let problemResult = transcribeDnaSeqToRnaSeq problemInput
+printfn "Problem result:"
+printfn "%s" problemResult
